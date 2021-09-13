772e48cac38515e46d3df065b97b1d8531e8747f
Tejas <tejas14all@gmail.com>
2021-09-13 17:57:06 +0530

    login +

--- jquery/ecom-app/assets/js/ecom.js
+++ jquery/ecom-app/assets/js/ecom.js
@@ -1,1 +1,16 @@
- 
+ function logout(){
+     localStorage.clear(); // remove all items from local storage 
+     toastr.success("You Will redirect to Login page soon!!!","Logout done",3000);
+    setTimeout(()=>{
+        location.href="Login.html";
+    },2000);
+     
+    //  localStorage.removeItem("authToken"); // remove only authToken from local storage 
+    
+}
+
+ $(document).ready(function(){
+    $("#logout").click(function(){
+        logout();
+    })
+})
