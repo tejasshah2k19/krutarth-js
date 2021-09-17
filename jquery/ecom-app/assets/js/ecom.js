9c32b090748219b1c02c93d425c712cdc509d376
Tejas <tejas14all@gmail.com>
2021-09-17 18:05:47 +0530

    add cart and admin can add user

--- jquery/ecom-app/assets/js/ecom.js
+++ jquery/ecom-app/assets/js/ecom.js
@@ -2,7 +2,7 @@
      localStorage.clear(); // remove all items from local storage 
      toastr.success("You Will redirect to Login page soon!!!","Logout done",3000);
     setTimeout(()=>{
-        location.href="Login.html";
+        location.href="/Login.html";
     },2000);
      
     //  localStorage.removeItem("authToken"); // remove only authToken from local storage 
